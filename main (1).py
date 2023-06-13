2621011e4c97814c40eb1cc1ea46e68677fedb0a
Nick  Murimi <126444800+NickMurimi@users.noreply.github.com>
2023-06-13 12:21:53 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+bestSeries = input ("what is your favourite best series? ")
+if bestSeries == "peaky blinders":
+  print("Yes, tell me why? ")
+  faveCharacter = input("who is your favourite character?" )
+  if faveCharacter == "Tommy Shelby":
+   print("Right answer")
+  else:
+    print("Nah, Tommy shelby's the greatest")
+elif bestSeries == "prison break":
+  print("also that great series comrade!")
+else:
+  print("You don't know any of your favourite best series?fuck off man!")
+
